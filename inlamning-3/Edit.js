718ade2704f7a0ed44e39d6792cc2c9ccd73792d
TLindskog <74720013+TLindskog@users.noreply.github.com>
2021-03-19 22:03:30 +0100

    Delete inlamning-3 directory

--- inlamning-3/Edit.js
+++
@@ -1,9 +0,0 @@
-const Edit = () => {
-    return (
-        <div className="edit">
-            <h3>Edit</h3>
-        </div>
-    );
-}
-
-export default Edit;
--- inlamning-3/dogList.js
+++
@@ -1,9 +0,0 @@
-import Dogs from './Dogs'
-
-export default function DogList({ values, toggleValue }) {
-  return (
-    values.map(value => {
-      return <Dogs key={value.id} toggleValue={toggleValue} value={value} />
-    })
-  )
-}
